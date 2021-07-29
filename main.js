fbf2e4e983bc1995a4f66e86e07adbc822e73eb0
my-rooftop <dgwogh@dgist.ac.kr>
2021-07-29 21:54:05 +0900

    Add posting

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
   <!doctype html>
   <html>
   <head>
-    <title>WEB1 - ${title}</title>
+    <title>WEB - ${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -35,7 +37,6 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    console.log(url.parse(_url, true));
     if(pathname === '/'){
       if(queryData.id == undefined){
 
@@ -61,8 +62,43 @@ var app = http.createServer(function(request,response){
         });
       });
       }
-    }
-    else{
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+
+        var title = 'WEB - create';
+
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `        
+        <form action="http://localhost:3000/create_process" method = "post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+            <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+        </form>
+        `);
+        response.end(template);
+        response.writeHead(200);
+      });
+
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`}); //writeHead 302는 다른곳으로 redirection 하라는 뜻.
+          response.end('success')
+        })
+      });
+      
+    }else {
       response.writeHead(404);
       response.end('Not found');
     }
