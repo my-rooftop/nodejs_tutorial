7f91bbea11cdf412b65aeb485f5dc04a1a86c126
my-rooftop <dgwogh@dgist.ac.kr>
2021-08-04 01:05:34 +0900

    update create/update/delete by sql

--- main.js
+++ main.js
@@ -28,20 +28,9 @@ var app = http.createServer(function(request,response){
     if(pathname === '/'){
       if(queryData.id == undefined){
 
-        // fs.readdir('./data', function(error, filelist){
-
-        //   var title = 'Welcome';
-        //   var description = 'Hello, Node.js';
-
-        //   var list = template.List(filelist);
-        //   var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
-        //   response.end(html);
-        //   response.writeHead(200);
-        // });
-
         //두번째 인자 topics에서 결과가 담기도록 약속되어있음.
-        DB.query(`SELECT * FROM topic`, function(error, topics, fields){
-          console.log(topics);
+        DB.query(`SELECT * FROM topic`, function(error, topics){
+          // console.log(topics);
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = template.List(topics);
@@ -52,36 +41,41 @@ var app = http.createServer(function(request,response){
 
 
       } else{
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base; // .. root 을 제외한 경로를 내뱉도록 함. parse중에 base에 해당한게 그런듯
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var sanitizedTitle = sanitizeHtml(title);
-          var sanitizeDescription = sanitizeHtml(description);// sanitize 함수로 출력 소독함. 출력 보안을 위해.
-          var list = template.List(filelist);
-          //delete의 경우 이를 링크로 처리할경우 보안문제 발생함. 따라서 post 방식으로 변경해야됨 
-          var html = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2>${sanitizeDescription}`, 
-          `
-          <a href="/create">create</a> 
-          <a href="/update?id=${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value = "delete">
-          </form>
-          `);
-          response.end(html);
+        DB.query(`SELECT * FROM topic`, function(error, topics){
+        if(error){
+          throw error;
+        }
+        //DB.query(`SELECT * FROM topic WHERE id=${queryData.id}`, function(error2, topic){ 이거보단 아래처럼 배열에 담아서 주면 보안성 증가함. ?에 오른쪽을 자동으로 대입하도록 되어있으며,
+        //또한 공격성이 있는것은 알아서 필터링 하는 역할도 함. 따라서 아래 방법이 좀 더 보안성 높음, 
+        DB.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+          if(error2){
+            throw error2;
+          }
+          //객체들이 배열형태로 들어오기때문에 index 써주어야함.
+          console.log(topic);
+          var title = topic[0].title;
+          var description = topic[0].description;
+          var list = template.List(topics);
+          var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,`
+              <a href="/create">create</a> 
+              <a href="/update?id=${queryData.id}">update</a>
+              <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${queryData.id}">
+                <input type="submit" value = "delete">
+              </form>`);
           response.writeHead(200);
+          response.end(html);
         });
       });
+
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-
-        var title = 'WEB - create';
-
-        var list = template.List(filelist);
-        //링크로 하면 누구나 외부에서 글을 수정할 수도 있기 때문에, post방식으로 진행함.
-        var html = template.HTML(title, list, `        
+      DB.query(`SELECT * FROM topic`, function(error, topics){
+        // console.log(topics);
+        var title = 'Create';
+        var list = template.List(topics);
+        var html = template.HTML(title, list, 
+        `        
         <form action="/create_process" method = "post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -91,11 +85,10 @@ var app = http.createServer(function(request,response){
             <input type="submit">
         </p>
         </form>
-        `, ``);
-        response.end(html);
+        `, `<a href="/create">create</a>`);
         response.writeHead(200);
+        response.end(html);
       });
-
     } else if(pathname === '/create_process'){
       var body = '';
       request.on('data', function(data){
@@ -103,30 +96,35 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
         var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`}); //writeHead 302는 다른곳으로 redirection 하라는 뜻.
-          response.end('success')
-        })
+        DB.query(`INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`, [post.title, post.description, 1], function(error, result){
+          if(error){
+            throw error;
+          }
+          response.writeHead(302, {Location: `/?id=${result.insertId}`});
+          response.end();
+        });
       });
       
-    }else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.List(filelist);
-          var html = template.HTML(title, list, 
+    } else if(pathname === '/update'){
+      DB.query('SELECT * FROM topic', function(error, topics){
+        if(error){
+          throw error;
+        }
+        DB.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+          if(error){
+            throw error;
+          }
+          var list = template.List(topics);
+          var html = template.HTML(topic[0].title, list, 
             //value = "${title}" 옵션 주면 placeholder 자리에 내용 채워줌.
             //textarea의 경우 html태그 안쪽에 넣어주면 placeholder 안에 내용 들어가게됨.
             //hidden의 경우 사용자가 제목을 변경할 수도 있기 때문에 원래 제목을 보존하기 위해서 생성, 히든처리하면 사용자에게는 안보이는 효과
           `
           <form action="/update_process" method = "post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <input type="hidden" name="id" value="${topic[0].id}">
+          <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
           <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
+              <textarea name="description" placeholder="description">${topic[0].description}</textarea>
           </p>
           <p>
               <input type="submit">
@@ -134,9 +132,9 @@ var app = http.createServer(function(request,response){
           </form>
           `
           
-          , `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
-          response.end(html);
+          , `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`);
           response.writeHead(200);
+          response.end(html);
         });
       });
     } else if(pathname === '/update_process'){
@@ -146,16 +144,9 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
         var post = qs.parse(body);
-        var id = post.id;
-        var filteredId = path.parse(id).base; // 루트 경로 거르고 접근할 수 있도록.
-        var title = post.title;
-        var description = post.description;
-        //rename을 통해 파일명 변경, 전칸을 후칸로 바꾸라는 뜻임.
-        fs.rename(`data/${filteredId}`, `data/${title}`, function(error){
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`}); //writeHead 302는 다른곳으로 redirection 하라는 뜻.
-            response.end('success');
-          });
+        DB.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id =?', [post.title, post.description, post.id], function(error, result){
+          response.writeHead(302, {Location: `/?id=${post.id}`}); //writeHead 302는 다른곳으로 redirection 하라는 뜻.
+          response.end();
         });
       });
     } else if(pathname === '/delete_process'){
@@ -168,7 +159,10 @@ var app = http.createServer(function(request,response){
         var id = post.id;
         var filteredId = path.parse(id).base; // 루트 경로 거르고 접근할 수 있도록.
         //삭제하는 방법 , unlink
-        fs.unlink(`data/${filteredId}`, function(error){
+        DB.query('DELETE FROM topic WHERE id = ?', [post.id], function(error, result){
+          if(error){
+            throw error;
+          }
           response.writeHead(302, {Location: `/`}); //writeHead 302는 다른곳으로 redirection 하라는 뜻.
           response.end();
         });
