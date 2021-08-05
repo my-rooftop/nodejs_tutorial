f11b9140c4b00d82ba094e0f4f8216a693724467
my-rooftop <dgwogh@dgist.ac.kr>
2021-08-05 14:39:02 +0900

    add author create/update/delete

--- main.js
+++ main.js
@@ -29,11 +29,19 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/update'){
       topic.update(request, response, queryData);
     } else if(pathname === '/update_process'){
-      topic.update_process(request, response, queryData);
+      topic.update_process(request, response);
     } else if(pathname === '/delete_process'){
       topic.delete_process(request, response);
     } else if(pathname === '/author'){
       author.home(request, response);
+    } else if(pathname === '/author/create_process'){
+      author.create_process(request, response);
+    } else if(pathname === '/author/update'){
+      author.update(request, response);
+    } else if(pathname === '/author/update_process'){
+      author.update_process(request, response);
+    } else if(pathname === '/author/delete_process'){
+      author.delete_process(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
